b7ca7c362f8ecb7b6c9641a3a523009f5c816e49
Amirthaa181 <samirtha79@gmail.com>
2021-02-13 11:18:45 +0530

    army

---
+++ src/org/sample/EmployeeDetails.java
@@ -0,0 +1,27 @@
+package org.sample;
+
+public class EmployeeDetails {
+
+	
+	//created method --------method definition
+	public void javaCourse() {
+		
+		System.out.println("Java class ...");
+	}
+	
+	private void pythonCourse() {
+		
+		System.out.println("Python Class..");
+	}
+	
+	//create a main method
+	public static void main(String[] args) {
+		
+		EmployeeDetails emp=new EmployeeDetails();
+		emp.pythonCourse();
+		emp.javaCourse();
+		
+		
+	}
+	
+}
